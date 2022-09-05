27f94414f9aedc549d01b6b6fe7b87fb9baeb4de
22005501YACHU <yachu84952@gmail.com>
2022-09-05 13:25:56 +0800

    add root controller

--- src/main/java/com/example/demo2/Demo2Application.java
+++ src/main/java/com/example/demo2/Demo2Application.java
@@ -2,12 +2,20 @@ package com.example.demo2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Demo2Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Demo2Application.class, args);
 	}
+	@GetMapping("/")
+	public String helloworld(){
+		return "hello world";
+	}
+
 
 }
